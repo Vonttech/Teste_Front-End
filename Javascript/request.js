58b2c7aa0e22194f4c14a395953797f2b8aee8f2
Victor Matheus <36866913+Vonttech@users.noreply.github.com>
2020-06-10 14:50:35 -0300

    Recuperação de dados
    Nova url para recuperar as músicas dos albuns

--- Javascript/request.js
+++ Javascript/request.js
@@ -1,9 +1,9 @@
-
 const url = 'https://itunes.apple.com/search?term=sia&entity=album'
+const url_musicas = 'https://itunes.apple.com/search?term=sia&entity=album&entity=song&limit=200'
 
 //Lidando com as requisições
 
-//Recuperando os dados
+//Recuperando os dados dos titulos e data
 let titulo_1 = document.querySelector('#titulo-1')
 let titulo_2 = document.querySelector('#titulo-2')
 
@@ -32,4 +32,46 @@ function trataAno(releaseDate) {
     var str = String(releaseDate)
     var ano = str.slice(0, 4)
     return ano
+}
+
+
+//Recuperando as musicas dos albuns
+fetch(url_musicas)
+    .then(resp => resp.json())
+    .then(function (data) {
+        let results = data.results
+        results.forEach(element => {
+            if (element.collectionName == 'Everyday Is Christmas') {
+                criaElementos('coluna-musicas-1', element)
+            }
+            if (element.collectionName == 'This Is Acting (Deluxe Version)') {
+                criaElementos('coluna-musicas-2', element)
+            }
+        });
+    })
+    .catch(error => console.log(error))
+
+function criaElementos(id, element) {
+    let div_pai = document.getElementById(id)
+
+    let trackName = element.trackName
+    //Conversão de milisegundos para minutos e segundos
+    let min = Math.floor(element.trackTimeMillis / 1000 / 60 << 0)
+    let sec = Math.floor(element.trackTimeMillis % 60)
+
+
+    let div = document.createElement('DIV')
+    let div_col_esquerda = document.createElement('DIV')
+    let div_col_direita = document.createElement('DIV')
+
+
+    div.className = 'row border-top border-bottom'
+    div_col_esquerda.className = 'col-10'
+    div_col_direita.className = 'col'
+
+    div_col_esquerda.innerHTML = trackName
+    div_col_direita.innerHTML = `${min}:${sec}`
+    div.appendChild(div_col_esquerda)
+    div.appendChild(div_col_direita)
+    div_pai.appendChild(div)
 }
